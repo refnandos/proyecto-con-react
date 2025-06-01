c17c84e0537008ebd72424017c24603c5327e3ab
refnandos <fernandorevollo10@gmail.com>
2025-06-02 01:22:41 +0200

    react/adaptacion se ha añadido varios componentes a el index

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
-import { PrimerComponente } from './components/PrimerComponente';
-import { SegundoComponente } from './components/SegundoComponente';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route, Routes} from 'react-router-dom';
+
+import { Header } from './components/Header';
+import { Navbar } from './components/Navbar';
+import { Home } from './components/Home';
+import { About } from './components/About';
+
+// import { Sidebar } from './components/Sidebar';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-        <PrimerComponente>
-        </PrimerComponente>
+  
+  const [showNav, setShowNav] = useState(true);
 
-        <SegundoComponente>
-          
-        </SegundoComponente>
-        
-        
-      </header>
-    </div>
+  return (
+    <>
+      <Router>
+        <Header>
+          <GiHamburgerMenu onClick={() => setShowNav(!showNav)}/>
+        </Header>
+        {/* <Sidebar /> */}
+        <Navbar show={showNav} />
+
+          <div className='main'>
+        <Routes>
+            <Route path='/' exact={true} Component={Home} />
+            <Route path='/about' exact={true} Component={About} />
+        </Routes>
+          </div>
+
+      </Router>
+    </>
   );
 }
 
