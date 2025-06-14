f277c85b4833c5a055da915f3f7baf80b27764c5
refnandos <fernandorevollo10@gmail.com>
2025-06-14 17:24:44 +0200

    nueva carpeta para imagenes y exportacion facilitada con in index.js

---
+++ src/images/pieces/index.js
@@ -0,0 +1,27 @@
+import TorreBlanco from './Torre-Blanco.png';
+import TorreNegro from './Torre-Negro.png';
+import CaballoBlanco from './Caballo-Blanco.png';
+import CaballoNegro from './Caballo-Negro.png';
+import AlfilBlanco from './Alfil-Blanco.png';
+import AlfilNegro from './Alfil-Negro.png';
+import ReyBlanco from './Rey-Blanco.png';
+import ReyNegro from './Rey-Negro.png';
+import ReinaBlanco from './Reina-Blanco.png';
+import ReinaNegro from './Reina-Negro.png';
+import PeonBlanco from './Peon-Blanco.png';
+import PeonNegro from './Peon-Negro.png';
+
+export const pieceImages = {
+    'torre-blanco': TorreBlanco,
+    'torre-negro': TorreNegro,
+    'caballo-blanco': CaballoBlanco,
+    'caballo-negro': CaballoNegro,
+    'alfil-blanco': AlfilBlanco,
+    'alfil-negro': AlfilNegro,
+    'rey-blanco': ReyBlanco,
+    'rey-negro': ReyNegro,
+    'reina-blanco': ReinaBlanco,
+    'reina-negro': ReinaNegro,
+    'peon-blanco': PeonBlanco,
+    'peon-negro': PeonNegro
+};
